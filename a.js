e1970c06b073cc6151c4b7431794095cce6a88c8
Siheon Jeon <wjstlgjs_21_@naver.com>
2023-05-03 20:59:52 +0900

    second commit

--- a.js
+++ a.js
@@ -2,12 +2,12 @@ let a = {
 
     "지브리OST리스트" : [
         {
-            "name" : "마녀 배달부 키키",
-            "song" : "따스함에 둘러쌓인다면"
+            "name" : "사과",
+            "color" : "Red"
         },
         {
-            "name" : "하울의 움직이는 성",
-            "song" : "1"
+            "name" : "포도",
+            "color" : "Purple"
         }
     ]
     

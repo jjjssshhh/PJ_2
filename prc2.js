38cedeae7c70b32fc2b0c51bd560867d3f8c845c
Siheon Jeon <wjstlgjs_21_@naver.com>
2023-05-06 19:17:52 +0900

    prc2 commit

---
+++ prc2.js
@@ -0,0 +1,17 @@
+let a = {
+
+    "가게물품" : [
+        {
+            "name" : "사과",
+            "color" : "Red"
+        },
+        {
+            "name" : "포도",
+            "color" : "Purple"
+        }
+    ]
+    
+}
+
+a = JSON.parse( a)
+console.log(a)
